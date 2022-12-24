e5d3a25ff8cf467f9929c3c611d15fbe0bdd105b
Avinash <119832504+Avinash-Athi@users.noreply.github.com>
2022-12-24 19:35:49 +0530

    Update admin.js

--- Server/Controllers/admin.js
+++ Server/Controllers/admin.js
@@ -1,8 +1,7 @@
 const Candidate = require("../Models/addcandidate");
 
-
 //ADD CANDIDATE SINGULAR
-const postAddCandidade = (req, res, next) => {
+const postAddCandidade =async (req, res, next) => {
  
     const CandidateName = req.body.CandidateName;
     const Email = req.body.Email;
@@ -16,6 +15,20 @@ const postAddCandidade = (req, res, next) => {
 
     if(!CandidateName||!Email||!Contact||!Role||!PAN||!Experience){
       return res.status(422).json({error:"Plz filled the field property"});
+    }
+
+    const cz = await Candidate.addCandid.find();
+    if (cz.find((cand) => cand.PAN === PAN)) {
+        //console.log(PAN);
+        return res.status(500).json({ message: "Candidate PAN already exists " });
+    }
+    if (cz.find((cand) => cand.Email === Email)) {
+      //console.log(PAN);
+      return res.status(500).json({ message: "Candidate Email already exists" });
+    }
+    if (cz.find((cand) => cand.Contact === Contact)) {
+      //console.log(PAN);
+      return res.status(500).json({ message: "Candidate Contact already exists" });
     }
 
     //counter
@@ -41,7 +54,10 @@ const postAddCandidade = (req, res, next) => {
         data.save().then(result => {
           console.log("Data saved");
         }).catch(err=> {
-          console.log(err);
+          console.log("__________________")
+          //console.log(err);
+          //res.status(422).json({err})
+          console.log("__________________")
         })
       }
     )
@@ -52,8 +68,33 @@ const postAddCandidade = (req, res, next) => {
 
 const postMultiple=async (req,res,next)=>{
 
-  var seqId=0;
+    var seqId=0;
 
+    const cz = await Candidate.addCandid.find();
+
+    // }
+    let flag=0;
+    req.body.data.map((candidate, index)=>{
+      //console.log(candidate[index].Email);
+      console.log("__________")
+      console.log(candidate.Email)
+      console.log(index);
+      console.log("__________")
+
+      if (cz.find((cand) => cand.Email === candidate.Email )) {
+        //console.log(PAN);
+        console.log(candidate.Email+" Exist !!")
+        flag=1;
+        return res.status(500).json({ message: `${candidate.Email} already exists` });
+      }
+
+
+
+
+    })
+    //console.log("mail : "+mail);
+
+    if(flag!=1){
     Candidate.countermodel.findOneAndUpdate(
     {id:"val1"},
     {"$inc":{"seq": req.body.data.length}},
@@ -70,18 +111,19 @@ const postMultiple=async (req,res,next)=>{
       let caninfo= req.body.data.map((candidate, index)=>{
         return {cid:"ZEN"+(index+parseInt(seqId)), ...candidate}
       })
-      // console.log(caninfo);
+      //console.log(caninfo);
 
       Candidate.addCandid.insertMany(caninfo,(err,data)=>{
         if(err){
             console.log(err);
         }else{
-          
-            res.json({message: "successfully submitted"})
             console.log("succesfully done")
+            return res.json({message: "successfully submitted"})
         }
       });
     })
+
+  }
 
 }
 
